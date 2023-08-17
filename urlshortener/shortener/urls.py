2895fa804c2946cfdd0f45aa21de00861d2b77e5
bermet <bermet.alibek@gmail.com>
2023-08-16 23:51:53 -0500

    added retrieving original url option

--- urlshortener/shortener/urls.py
+++ urlshortener/shortener/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, retrieve_original_url
+from .views import index, retrieve_original_url, retrieve_shorturl
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('<str:shorturl>/', retrieve_original_url, name='longurl'),
+    path('retrieve/', retrieve_shorturl, name='retrieve_shorturl'),
 ]
