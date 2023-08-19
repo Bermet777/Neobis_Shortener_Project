40a25d6e222b9d4e1800c287c4ec056a846bb8f3
bermet <bermet.alibek@gmail.com>
2023-08-18 23:53:32 -0500

    adding retrieve original url view

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -5,6 +5,18 @@ from .models import Urls
 from .serializers import urlShortenerSerializer
 from rest_framework import generics
 
+import random
+
+# class ShortenedURLCreateView(generics.CreateAPIView):
+#     queryset = Urls.objects.all()
+#     serializer_class = urlShortenerSerializer
+
+# class ShortenedURLRetrieveView(generics.RetrieveAPIView):
+#     queryset = Urls.objects.all()
+#     serializer_class = urlShortenerSerializer
+#     lookup_field = 'short_code'
+
+# urlshortener_app/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
@@ -12,7 +24,7 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from .models import Urls
 from .serializers import urlShortenerSerializer
 import hashlib
-
+# Counter to ensure uniqueness of short codes
 url_counter = 0
 
 @api_view(['GET', 'POST'])
@@ -35,11 +47,25 @@ def retrieve_original_url(request, shorturl):
     serializer = urlShortenerSerializer(shortened_url)
     return Response(serializer.data)
 
-
+# Updated URL shortening function to ensure uniqueness
 def shorten_url(longurl, counter):
-  
+    # Generate a hash of the original URL
     hash_code = hashlib.md5(longurl.encode()).hexdigest()
 
-    
+    # Append counter and random characters to ensure uniqueness
     shorturl = f"{hash_code[:6]}{counter:03}"
     return shorturl
+
+@api_view(['GET', 'POST'])
+def retrieve_shorturl(request):
+    if request.method == 'POST':
+        shorturl = request.data.get('shorturl')
+        try:
+            shortened_url = Urls.objects.get(shorturl=shorturl)
+            original_url = shortened_url.longurl
+        except Urls.DoesNotExist:
+            original_url = None
+
+        return render(request, 'retrieve_shorturl.html', {'original_url': original_url})
+
+    return render(request, 'retrieve_shorturl.html')
